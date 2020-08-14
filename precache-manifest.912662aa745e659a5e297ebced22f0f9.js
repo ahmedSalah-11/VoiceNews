f2d3a3b8284fdc2d89d59a1e88fb73b5ffa5f03b
ahmedSalah-11 <ahmedsalah@stud.cu.edu.eg>
2020-08-14 14:37:39 +0200

    Updates

---
+++ precache-manifest.912662aa745e659a5e297ebced22f0f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa215e077990668a6987901467109c8",
+    "url": "/VoiceNews/index.html"
+  },
+  {
+    "revision": "c7d5c52f4e8ab00ac108",
+    "url": "/VoiceNews/static/css/main.8a79608c.chunk.css"
+  },
+  {
+    "revision": "9e161cb5ba53b7449412",
+    "url": "/VoiceNews/static/js/2.7344126b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/VoiceNews/static/js/2.7344126b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d5c52f4e8ab00ac108",
+    "url": "/VoiceNews/static/js/main.763edf8c.chunk.js"
+  },
+  {
+    "revision": "69e20e3a03a32574d637",
+    "url": "/VoiceNews/static/js/runtime-main.10e0ec13.js"
+  },
+  {
+    "revision": "a08d098ee03fe81a8ce39b0605573d07",
+    "url": "/VoiceNews/static/media/voice5.a08d098e.png"
+  }
+]);
